79102c723fb2ab010fac5d5b60407316ee0af9fb
shahidul shohid <shahidulislamshohid7gmail.com>
2024-12-12 22:29:06 +0600

    jobApply page is done

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,68 +1,91 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import useAuth from "../../hooks/UseAuth";
+import Swal from "sweetalert2";
 
 const JobApply = () => {
-    const {id} = useParams()
-    console.log(id)
-    const submitJobApplyCation = (e) => {
-        e.preventDefault()
-        const form = e.target
-        const linkedIn = form.linkedIn.value 
-        const gitHub = form.gitHub.value 
-        const resume = form.resume.value 
-        console.log(linkedIn, gitHub, resume)
-    }
+  const { id } = useParams();
+  const { user } = useAuth();
+  const navigate = useNavigate()
+  // console.log(id, user)
+  const submitJobApplyCation = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
+    const gitHub = form.gitHub.value;
+    const resume = form.resume.value;
+    // console.log(linkedIn, gitHub, resume)
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user?.email,
+      linkedIn,
+      gitHub,
+      resume,
+    };
+    fetch("http://localhost:5000/job-applications", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(jobApplication),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          navigate('/myApplications')
+        }
+      });
+  };
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+    <div className="card bg-base-100 w-full shadow-2xl">
+      <h1 className="text-5xl font-bold">Apply Job and Good Luch!</h1>
+      <form onSubmit={submitJobApplyCation} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">LinkedIn URL</span>
+          </label>
+          <input
+            type="url"
+            placeholder="LinkedIn URL"
+            name="linkedIn"
+            className="input input-bordered"
+            required
+          />
         </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form onSubmit={submitJobApplyCation} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">LinkedIn URL</span>
-              </label>
-              <input
-                type="url"
-                placeholder="LinkedIn URL" name="linkedIn"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">GitHub URl</span>
-              </label>
-              <input
-                type="url" name="gitHub"
-                placeholder="GitHub URl"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Resume URl</span>
-              </label>
-              <input
-                type="url"
-                placeholder="Resume URl" name="resume"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Apply</button>
-            </div>
-          </form>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">GitHub URl</span>
+          </label>
+          <input
+            type="url"
+            name="gitHub"
+            placeholder="GitHub URl"
+            className="input input-bordered"
+            required
+          />
         </div>
-      </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Resume URl</span>
+          </label>
+          <input
+            type="url"
+            placeholder="Resume URl"
+            name="resume"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Apply</button>
+        </div>
+      </form>
     </div>
   );
 };
